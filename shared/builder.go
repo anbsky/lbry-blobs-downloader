8fff2ef4e05e725cd9c87dbd2b74f4b0933f3738
Andriy Biletsky <andrey@vr2.net>
2025-05-02 01:20:36 +0700

    Do not use system temp dir for blob processing

--- shared/builder.go
+++ shared/builder.go
@@ -11,7 +11,12 @@ import (
 )
 
 func BuildStream(sdBlob *stream.SDBlob, fileName string, destinationPath string, blobsDirectory string) error {
-	tmpDir := os.TempDir()
+	tmpDir, err := os.MkdirTemp(blobsDirectory, "blobsTmp*")
+	if err != nil {
+		return errors.Err(err)
+	}
+	defer os.RemoveAll(tmpDir)
+
 	tmpName := path.Join(tmpDir, fileName+".tmp")
 	finalName := path.Join(destinationPath, fileName)
 	f, err := os.Create(tmpName)
